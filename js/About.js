c0233027a75ec6803a35811bb9422f5f0c4a1af0
T0R7AS <teletabispirmasis610@gmail.com>
2021-02-15 19:05:29 +0200

    comments

---
+++ js/About.js
@@ -0,0 +1,25 @@
+ /* ----------------- IMPORTS ----------------- */
+ /* header */
+ /* hero */
+ /* about me */
+ /* services */
+ /* works */
+ /* professional skills */
+ /* testimonials */
+ /* blogs */
+ /* contact me */
+ /* footer */
+ /* left ones */
+
+ /* ----------------- CODING ----------------- */
+ /* header */
+ /* hero */
+ /* about me */
+ /* services */
+ /* works */
+ /* professional skills */
+ /* testimonials */
+ /* blogs */
+ /* contact me */
+ /* footer */
+ /* left ones */
