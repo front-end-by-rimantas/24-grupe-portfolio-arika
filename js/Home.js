dce311362842347b98c26ce9519d1ec9b71108eb
T0R7AS <teletabispirmasis610@gmail.com>
2021-02-21 19:39:54 +0200

    Images Gallery

--- js/Home.js
+++ js/Home.js
@@ -1,6 +1,18 @@
 //   ----------------- IMPORTS -----------------
 //   header
 //   hero
+//   about me
+//   services
+//   works
+//   professional skills
+//   testimonials
+//   blogs
+//   contact me
+//   footer
+
+//   ----------------- CODING -----------------
+//   header
+//   hero
 const TypeWriter = function(txtElement, words, wait = 500) {
     this.txtElement = txtElement;
     this.words = words;
@@ -57,18 +69,61 @@ function init() {
 //   about me
 //   services
 //   works
-//   professional skills
-//   testimonials
-//   blogs
-//   contact me
-//   footer
+const gallery = document.querySelectorAll(".row .img-box");
+const previewBox = document.querySelector(".preview-box");
+const previewImg = previewBox.querySelector("img");
+const closeIcon = previewBox.querySelector(".icon");
+const currentImg = previewBox.querySelector(".current");
+const totalImg = previewBox.querySelector(".total");
+const background = document.querySelector(".background");
+const dontAppear = document.querySelectorAll("h2");
 
-//   ----------------- CODING -----------------
-//   header
-//   hero
-//   about me
-//   services
-//   works
+
+window.onload = ()=>{
+    for (let i = 0; i < gallery.length; i++) {
+        totalImg.textContent = gallery.length;
+        let  newIndex = i;
+        gallery[i].onclick = ()=>{
+            function preview() {
+                currentImg.textContent = newIndex + 1;
+                let selectedImgUrl = gallery[newIndex].querySelector("img").src;
+                previewImg.src = selectedImgUrl;
+            }
+
+            const prevBtn = document.querySelector(".prev");
+            const nextBtn = document.querySelector(".next");
+            prevBtn.onclick = ()=>{
+                newIndex--;
+                if (newIndex === -1) {
+                    newIndex = 8;
+                    preview();
+                }
+                preview();
+            }
+            nextBtn.onclick = ()=>{
+                newIndex++;
+                if (newIndex === 9) {
+                    newIndex = 0;
+                    preview();
+                }
+                preview();
+            }
+
+            preview();
+            previewBox.classList.add("show");
+            background.style.display = "block";
+
+            closeIcon.onclick = ()=>{
+                previewBox.classList.remove("show");
+                background.style.display = "none";
+            }
+            background.onclick = ()=>{
+                previewBox.classList.remove("show");
+                background.style.display = "none";
+            }
+        }
+    }
+}
 //   professional skills
 //   testimonials
 //   blogs
